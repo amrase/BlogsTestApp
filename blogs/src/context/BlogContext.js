9679c11370e638f867aaaedd7c1a6890789b5437
Amra Sezairi <amra.sezairi@hotmail.com>
2020-03-18 16:22:22 -0400

    Created BlogContext

---
+++ blogs/src/context/BlogContext.js
@@ -0,0 +1,21 @@
+import React,{useState} from 'react'
+
+const BlogContext = React.createContext()
+
+export const BlogProvider = ({children}) =>{
+
+    const [blogPosts,setBlogPost]=useState([])
+
+    const addBlogPost =  () =>{
+        setBlogPost(
+            [...blogPosts,
+                {title:`BlogPost #${blogPosts.length + 1}`}
+            ])
+    }
+
+    return <BlogContext.Provider value={{ data: blogPosts , addBlogPost: addBlogPost}}>
+        {children}
+    </BlogContext.Provider>
+}
+
+export default BlogContext
